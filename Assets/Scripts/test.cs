599456adf11192a38dc20e7f0aa5cbba550dff3c
PLikii <marioua672@gmail.com>
2022-05-10 00:27:12 +0300

    fix

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeFiled]
+    private int lives = 100;
+
+    [SerializeFiled]
+    private float speed = 3.0F;
+
+    [SerializeFiled]
+    private float jupmForce = 15.0F;
+
+
+    new private Rigidbody2D rigidbody;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+
+    private void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if(Impute.GetButton("Horizontal")) Run();
+        if(Impute.GetButtonDown("Jump")) Jump();
+    }
+
+    private void Run()
+    {
+        Vector3 direction = transform.right * Imput.GetAxeis("Horizontal");
         
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+    }
+    
+    private void Jump()
+    {
+        rigidbody.AddForce(transform.up * jupmForce, ForceMode2D.Impulse);
     }
 }
