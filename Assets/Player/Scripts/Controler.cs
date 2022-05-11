523447205824999eef0aaac5f03202d339104f72
PLikii <marioua672@gmail.com>
2022-05-11 15:56:01 +0300

    Добавив бонус скорість бігу на shift

--- Assets/Player/Scripts/Controler.cs
+++ Assets/Player/Scripts/Controler.cs
@@ -8,9 +8,12 @@ public class Controler : MonoBehaviour
     //in order to control both players using 1 script.
     public int playerIdx;
 
+
     //Variables.
-    public float movementSpeed = 500f;
-    public float jumpForce = 20f;
+    public float shiftSpeed;
+    public float movementSpeed;
+    public float jumpForce;
+    private float speed;
 
     //Ground stuff.
     public LayerMask whatIsGround;
@@ -45,23 +48,35 @@ public class Controler : MonoBehaviour
     void Update()
     {
 
-    
+    if (Input.GetKey(KeyCode.LeftShift))
+            speed = shiftSpeed;
+
+    if(!Input.GetKey(KeyCode.LeftShift))
+            speed = movementSpeed;
+        
     if (Input.GetKey(KeyCode.A))
         Move(-1);
+
     if (Input.GetKey(KeyCode.D))
         Move(1);
+
     if (Input.GetKeyDown(KeyCode.W))
         Jump();
+
     if (Input.GetKeyDown(KeyCode.Space))
         Jump();
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        Move(-1);
+    
+
+    if (Input.GetKey(KeyCode.LeftArrow))
+    Move(-1);
+
     if (Input.GetKey(KeyCode.RightArrow))
         Move(1);
+
     if (Input.GetKeyDown(KeyCode.UpArrow))
         Jump();
-        
+
     }
 
 
@@ -87,9 +102,9 @@ public class Controler : MonoBehaviour
 
         //if player is not moving, slow them down.
         if (rb.velocity.x > 0.2f)
-            rb.AddForce(movementSpeed * Time.deltaTime * -Vector2.right);
+            rb.AddForce(speed * Time.deltaTime * -Vector2.right);
         if (rb.velocity.x < -0.2f)
-            rb.AddForce(movementSpeed * Time.deltaTime * Vector2.right);
+            rb.AddForce(speed * Time.deltaTime * Vector2.right);
     }
 
     public void Move(int dir)
@@ -103,15 +118,15 @@ public class Controler : MonoBehaviour
         float xVel = rb.velocity.x;//Get x velocity.
 
         if (dir > 0)
-            rb.AddForce(movementSpeed * Time.deltaTime * Vector2.right * dir);
+            rb.AddForce(speed * Time.deltaTime * Vector2.right * dir);
         else if (dir < 0)
-            rb.AddForce(movementSpeed * Time.deltaTime * Vector2.right * dir);
+            rb.AddForce(speed * Time.deltaTime * Vector2.right * dir);
 
         //Help player turn around faster.
         if (xVel > 0.2f && dir < 0)
-            rb.AddForce(movementSpeed * 3.2f * Time.deltaTime * -Vector2.right);
+            rb.AddForce(speed * 3.2f * Time.deltaTime * -Vector2.right);
         if (xVel < 0.2f && dir > 0)
-            rb.AddForce(movementSpeed * 3.2f * Time.deltaTime * Vector2.right);
+            rb.AddForce(speed * 3.2f * Time.deltaTime * Vector2.right);
     }
 
     private void Flips(int dir)
